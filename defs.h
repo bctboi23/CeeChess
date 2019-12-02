c31eaad952066d9369690b11b175e6cea43c76eb
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 11:51:41 -0600

    Update defs.h

--- defs.h
+++ defs.h
@@ -90,7 +90,8 @@ typedef struct {
 /* MACROS */
 
 #define FR2SQ(f,r) ( (21 + (f) ) + ( (r) * 10 ) )
-#define SQ64(sq120) Sq120ToSq64[sq120]
+#define SQ64(sq120) (Sq120ToSq64[(sq120)])
+#define SQ120(sq64) (Sq64ToSq120[(sq64)])
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b)
 #define CLRBIT(bb,sq) ( (bb) &= ClearMask[ (sq) ])
