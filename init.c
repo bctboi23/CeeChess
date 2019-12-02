c90decc54012166e9d1b84037c8b267f631b6199
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 12:50:34 -0600

    Update init.c

--- init.c
+++ init.c
@@ -1,10 +1,36 @@
 #include "defs.h"
+#include "stdlib.h"
+
+#define RAND_64 ( (U64)rand() + \
+                  (U64)rand() << 15 + \
+                  (U64)rand() << 30 + \
+                  (U64)rand() << 45 + \
+                  ((U64)rand() & 0xf) << 60 )
 
 int Sq120ToSq64[BRD_SQ_NUM];
 int Sq64ToSq120[64];
 
 U64 SetMask[64];
 U64 ClearMask[64];
+
+U64 PieceKeys[13][120];
+U64 SideKey;
+U64 CastleKeys[16];
+
+void InitHashKeys() {
+  
+  int index = 0;
+  int index2 = 0;
+  for (index = 0; index < 13; ++index) {
+    for (index2 = 0; index2 < 120; ++index2) {
+      PieceKeys[index][inedx2] = RAND_64;
+    }
+  }
+  SideKey = RAND_64;
+  for (index = 0; index < 16; ++index) {
+    CastleKeys[index] = RAND_64;
+  }
+}
 
 void initBitMasks() {
   int index = 0;
