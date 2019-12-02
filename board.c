ff6dabb450de1c85ea2a8ddde0044c9abd27d081
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 13:30:31 -0600

    Update board.c

--- board.c
+++ board.c
@@ -1,40 +1,108 @@
 #include "stdio.h"
 #include "defs.h"
 
-void ResetBoard(S_BOARD, *pos) {
+int Parse_Fen(char *fen, S_BOARD *pos) {
+	
+	ASSERT(fen!=NULL);
+	ASSERT(pos!=NULL);
+	
+	int  rank = RANK_8;
+    int  file = FILE_A;
+    int  piece = 0;
+    int  count = 0;
+    int  i = 0; 
+	int  sq64 = 0; 
+	int  sq120 = 0;
+	
+	ResetBoard(pos);
+	
+	while ((rank >= RANK_1) && *fen) {
+	    count = 1;
+		switch (*fen) {
+            case 'p': piece = bP; break;
+            case 'r': piece = bR; break;
+            case 'n': piece = bN; break;
+            case 'b': piece = bB; break;
+            case 'k': piece = bK; break;
+            case 'q': piece = bQ; break;
+            case 'P': piece = wP; break;
+            case 'R': piece = wR; break;
+            case 'N': piece = wN; break;
+            case 'B': piece = wB; break;
+            case 'K': piece = wK; break;
+            case 'Q': piece = wQ; break;
 
-  int index = 0;
-  
-  for (index = 0; index < BRD_SQ_NUM; ++index) {
-    pos->pieces[index] = OFFBOARD;
-  }
-  
-  for (index = 0; index < 64; ++index) {
-    pos->pieces[SQ120(index)] = EMPTY;
-  }
-  
-  for (index = 0; index < 3; ++index) {
-    pos->bigPce[index] = 0;
-    pos->majPce[index] = 0;
-    pos->minPce[index] = 0;
-    pos->pawns[index] = 0ULL;
-  }
-  
-  for (index = 0; index < 12; ++index) {
-    pos->pceNum[index] = 0;
-  }
-  
-  pos->KingSq[WHITE] = pos->KingSq[BLACK] = NO_SQ;
-  
-  pos->side = BOTH;
-  pos->enPas = NO_SQ;
-  pos->fiftyMove = 0;
-  
-  pos->ply = 0;
-  pos->hisPly = 0;
-  
-  pos->castlePerm = 0;
-  
-  pos->posKey = 0ULL;
-  
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+                piece = EMPTY;
+                count = *fen - '0';
+                break;
+
+            case '/':
+            case ' ':
+                rank--;
+                file = FILE_A;
+                fen++;
+                continue;              
+
+            default:
+                printf("FEN error \n");
+                return -1;
+        }		
+		
+		for (i = 0; i < count; i++) {			
+            sq64 = rank * 8 + file;
+			sq120 = SQ120(sq64);
+            if (piece != EMPTY) {
+                pos->pieces[sq120] = piece;
+            }
+			file++;
+        }
+		fen++;
+	}
+	
+}
+
+void ResetBoard(S_BOARD *pos) {
+
+	int index = 0;
+	
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		pos->pieces[index] = OFFBOARD;
+	}
+	
+	for(index = 0; index < 64; ++index) {
+		pos->pieces[SQ120(index)] = EMPTY;
+	}
+	
+	for(index = 0; index < 3; ++index) {
+		pos->bigPce[index] = 0;
+		pos->majPce[index] = 0;
+		pos->minPce[index] = 0;
+		pos->pawns[index] = 0ULL;
+	}
+	
+	for(index = 0; index < 13; ++index) {
+		pos->pceNum[index] = 0;
+	}
+	
+	pos->KingSq[WHITE] = pos->KingSq[BLACK] = NO_SQ;
+	
+	pos->side = BOTH;
+	pos->enPas = NO_SQ;
+	pos->fiftyMove = 0;
+	
+	pos->ply = 0;
+	pos->hisPly = 0;
+	
+	pos->castlePerm = 0;
+	
+	pos->posKey = 0ULL;
+	
 }
