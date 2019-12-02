9c0789e81e8c2c0473cf915b4a4c11e472004601
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 12:27:39 -0600

    Update board.c

--- board.c
+++ board.c
@@ -5,4 +5,12 @@ void ResetBoard (S_BOARD, *pos) {
 
   int index = 0;
   
+  for (index = 0; index < BRD_SQ_NUM; ++index) {
+    pos->pieces[index] = OFFBOARD;
+  }
+  
+  for (index = 0; index < 64; ++index) {
+    pos->pieces[SQ120(index)] = EMPTY;
+  }
+  
 }
