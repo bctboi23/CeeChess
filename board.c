c9e951d167b02908f353598b1854644dd4f25179
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 12:33:45 -0600

    Update board.c

--- board.c
+++ board.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "defs.h"
 
-void ResetBoard (S_BOARD, *pos) {
+void ResetBoard(S_BOARD, *pos) {
 
   int index = 0;
   
@@ -12,5 +12,29 @@ void ResetBoard (S_BOARD, *pos) {
   for (index = 0; index < 64; ++index) {
     pos->pieces[SQ120(index)] = EMPTY;
   }
+  
+  for (index = 0; index < 3; ++index) {
+    pos->bigPce[index] = 0;
+    pos->majPce[index] = 0;
+    pos->minPce[index] = 0;
+    pos->pawns[index] = 0ULL;
+  }
+  
+  for (index = 0; index < 12; ++index) {
+    pos->pceNum[index] = 0;
+  }
+  
+  pos->KingSq[0] = pos->KingSq[0] = NO_SQ;
+  
+  pos->side = BOTH;
+  pos->enPas = NO_SQ;
+  pos->fiftyMove = 0;
+  
+  pos->ply = 0;
+  pos->hisPly = 0;
+  
+  pos->castlePerm = 0;
+  
+  pos->posKey = 0ULL;
   
 }
