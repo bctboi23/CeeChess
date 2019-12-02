f9fb2f9617e4237af392fd13c91076df10f09fc7
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-02 11:49:14 -0600

    Update seeChess.c

--- seeChess.c
+++ seeChess.c
@@ -5,8 +5,25 @@
 int main() {
 
   AllInit();
-  int index = 0;
-  U64 playBitBoard = 0ULL;
-
+  
+  int pieceOne = rand();
+  int pieceTwo = rand();
+  int pieceThree = rand();
+  int pieceFour = rand();
+  
+  printf("Piece 1:%X\n", pieceOne);
+  printf("Piece 2:%X\n", pieceTwo);
+  printf("Piece 3:%X\n", pieceThree);
+  printf("Piece 4:%X\n", pieceFour);
+  
+  int Key = PieceOne ^ PieceTwo ^ PieceThree ^ PieceFour;
+  int TempKey = PieceOne;
+  TempKey ^= PieceTwo;
+  TempKey ^= PieceThree;
+  TempKey ^= PieceFour;
+  
+  printf("Key:%X\n",Key);
+  printf("TempKey:%X\n",TempKey);
+  
   return 0;
 }
