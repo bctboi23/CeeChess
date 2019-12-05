1f7bbaa3e7431b333974ac9cadb11f09efb44273
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-05 09:21:31 -0600

    Update seeChess.c

--- seeChess.c
+++ seeChess.c
@@ -6,6 +6,7 @@
 
 #define PAWNMOVESW "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
 #define PAWNMOVESB "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
+#define KNIGHTSKINGS "5k2/1n6/4n3/6N1/8/3N4/8/5K2 w - - 0 1"
 
 int main() {
 
@@ -14,7 +15,7 @@ int main() {
 	S_BOARD board[1];
 	S_MOVELIST list[1];
 
-	ParseFen(PAWNMOVESB, board);
+	ParseFen(KNIGHTSKINGS, board);
 	PrintBoard(board);
 
 	GenerateAllMoves(board, list);
