13b664244264f2ea676e516b881f410b0fe9413f
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-04 23:12:50 -0600

    Finished Pawn Move Generation

--- movegen.c
+++ movegen.c
@@ -37,7 +37,7 @@ void AddEnPassantMove(const S_BOARD *pos, int move, S_MOVELIST *list) {
 
 }
 
-void addWhitePawnCapMove( const S_BOARD *pos, const int from, const int to, const int cap, S_MOVELIST *list ) {
+void AddWhitePawnCapMove( const S_BOARD *pos, const int from, const int to, const int cap, S_MOVELIST *list ) {
   if (RanksBrd[from] == RANK_7) {
     AddCaptureMove(pos, MOVE(from, to, cap, wQ, 0), list);
     AddCaptureMove(pos, MOVE(from, to, cap, wR, 0), list);
@@ -49,12 +49,36 @@ void addWhitePawnCapMove( const S_BOARD *pos, const int from, const int to, cons
   }
 }
 
-void addWhitePawnMove( const S_BOARD *pos, const int from, const int to, S_MOVELIST *list ) {
+void AddWhitePawnMove( const S_BOARD *pos, const int from, const int to, S_MOVELIST *list ) {
   if (RanksBrd[from] == RANK_7) {
     AddQuietMove(pos, MOVE(from, to, EMPTY, wQ, 0), list);
     AddQuietMove(pos, MOVE(from, to, EMPTY, wR, 0), list);
     AddQuietMove(pos, MOVE(from, to, EMPTY, wB, 0), list);
     AddQuietMove(pos, MOVE(from, to, EMPTY, wN, 0), list);
+  }
+  else {
+    AddQuietMove(pos, MOVE(from, to, EMPTY, EMPTY, 0), list);
+  }
+}
+
+void AddBlackPawnCapMove( const S_BOARD *pos, const int from, const int to, const int cap, S_MOVELIST *list ) {
+  if (RanksBrd[from] == RANK_2) {
+    AddCaptureMove(pos, MOVE(from, to, cap, bQ, 0), list);
+    AddCaptureMove(pos, MOVE(from, to, cap, bR, 0), list);
+    AddCaptureMove(pos, MOVE(from, to, cap, bB, 0), list);
+    AddCaptureMove(pos, MOVE(from, to, cap, bN, 0), list);
+  }
+  else {
+    AddCaptureMove(pos, MOVE(from, to, cap, EMPTY, 0), list);
+  }
+}
+
+void AddBlackPawnMove( const S_BOARD *pos, const int from, const int to, S_MOVELIST *list ) {
+  if (RanksBrd[from] == RANK_2) {
+    AddQuietMove(pos, MOVE(from, to, EMPTY, bQ, 0), list);
+    AddQuietMove(pos, MOVE(from, to, EMPTY, bR, 0), list);
+    AddQuietMove(pos, MOVE(from, to, EMPTY, bB, 0), list);
+    AddQuietMove(pos, MOVE(from, to, EMPTY, bN, 0), list);
   }
   else {
     AddQuietMove(pos, MOVE(from, to, EMPTY, EMPTY, 0), list);
@@ -78,17 +102,17 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
       ASSERT(SqOnBoard(sq));
 
       if (pos->pieces[sq + 10] == EMPTY) {
-        addWhitePawnMove(pos, sq, sq + 10, list);
+        AddWhitePawnMove(pos, sq, sq + 10, list);
         if (RanksBrd[sq] == RANK_2 && pos->pieces[sq + 20] == EMPTY) {
           AddQuietMove(pos, MOVE(sq, (sq + 20), EMPTY, EMPTY, MFLAGPS), list);
         }
       }
 
-      if (!SQOFFBOARD(sq) && PieceCol[pos->pieces[sq + 9]] == BLACK) {
-        addWhitePawnCapMove(pos, sq, sq+9, pos->pieces[sq + 9], list);
+      if (!SQOFFBOARD(sq + 9) && PieceCol[pos->pieces[sq + 9]] == BLACK) {
+        AddWhitePawnCapMove(pos, sq, sq + 9, pos->pieces[sq + 9], list);
       }
-      if (!SQOFFBOARD(sq) && PieceCol[pos->pieces[sq + 11]] == BLACK) {
-        addWhitePawnCapMove(pos, sq, sq+11, pos->pieces[sq + 11], list);
+      if (!SQOFFBOARD(sq + 11) && PieceCol[pos->pieces[sq + 11]] == BLACK) {
+        AddWhitePawnCapMove(pos, sq, sq + 11, pos->pieces[sq + 11], list);
       }
 
       if (sq + 9 == pos->enPas) {
@@ -100,7 +124,30 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
     }
   }
   else {
-    
-  }
+    for (pceNum = 0; pceNum < pos->pceNum[bP]; ++pceNum) {
+      sq = pos->pList[bP][pceNum];
+      ASSERT(SqOnBoard(sq));
 
+      if (pos->pieces[sq - 10] == EMPTY) {
+        AddBlackPawnMove(pos, sq, sq - 10, list);
+        if (RanksBrd[sq] == RANK_7 && pos->pieces[sq - 20] == EMPTY) {
+          AddQuietMove(pos, MOVE(sq, (sq - 20), EMPTY, EMPTY, MFLAGPS), list);
+        }
+      }
+
+      if (!SQOFFBOARD(sq - 9) && PieceCol[pos->pieces[sq - 9]] == WHITE) {
+        AddBlackPawnCapMove(pos, sq, sq - 9, pos->pieces[sq - 9], list);
+      }
+      if (!SQOFFBOARD(sq - 11) && PieceCol[pos->pieces[sq - 11]] == WHITE) {
+        AddBlackPawnCapMove(pos, sq, sq - 11, pos->pieces[sq - 11], list);
+      }
+
+      if (sq - 9 == pos->enPas) {
+        AddCaptureMove(pos, MOVE(sq, sq - 9, EMPTY, EMPTY, MFLAGEP), list);
+      }
+      if (sq - 11 == pos->enPas) {
+        AddCaptureMove(pos, MOVE(sq, sq - 11, EMPTY, EMPTY, MFLAGEP), list);
+      }
+    }
+  }
 }
--- seeChess.c
+++ seeChess.c
@@ -4,7 +4,8 @@
 #include "defs.h"
 #include "stdlib.h"
 
-#define PAWNMOVES "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define PAWNMOVESW "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define PAWNMOVESB "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1"
 
 int main() {
 
@@ -12,7 +13,7 @@ int main() {
 
 	S_BOARD board[1];
 
-	ParseFen(PAWNMOVES, board);
+	ParseFen(PAWNMOVESB, board);
 	PrintBoard(board);
 
 	S_MOVELIST list[1];
