155d0b3fb7ab82a0588468c79b8d5a1d87c0198f
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-06 10:40:55 -0600

    Update search.c

--- search.c
+++ search.c
@@ -3,6 +3,19 @@
 #include "stdio.h"
 #include "defs.h"
 
+int IsRepetition(const S_BOARD *pos) {
+	
+	int index = 0;
+	
+	for (index = pos->hisPly - pos->fiftyMove; index < pos->hisPly - 1; ++index) {
+		if (pos->posKey == pos->history[index].posKey;) {
+			return TRUE;
+		}
+	}
+	
+	return FALSE;
+}
+
 void SearchPosition(S_BOARD *pos) {
 
 }
