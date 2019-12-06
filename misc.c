5183751bb684b6358414739085b4d7864e3d925c
20reinitzt <35770571+20reinitzt@users.noreply.github.com>
2019-12-06 10:45:53 -0600

    Update misc.c

--- misc.c
+++ misc.c
@@ -2,3 +2,19 @@
 
 #include "stdio.h"
 #include "defs.h"
+
+#ifdef WIN32
+#include "windows.h"
+#else
+#include "time.h"
+#endif
+
+int GetTImeMs() {
+#ifdef WIN32
+  return GetTickCount();
+#else
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec * 1000 + t.tv_usec / 1000;
+#endif
+}
